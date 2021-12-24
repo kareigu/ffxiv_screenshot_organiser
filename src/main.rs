2020e496d002ed5144fe32bbb73da9b6964fd6da
mxr <eetu.kivale@gmail.com>
2021-12-24 16:07:43 +0200

    regex implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+use regex::Regex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let rgx = Regex::new(r#"(_[0-1][0-9][0-3][0-9][0-9]{4}_).+(\.png|jpg)"#)
+        .expect("Error constructing regex");
+
+    let files = std::fs::read_dir("screenshots")
+        .expect("Error opening dir")
+        .filter(|f| {
+            match f {
+                Ok(file) => rgx.is_match(file.file_name())
+            }
+        });
+    
+    for f in files {
+        println!("{:?}", f);
+    }
 
     Ok(())
 }
