fbabb2c2915e7560893e183d97f228b0bea13fa7
mxr <eetu.kivale@gmail.com>
2021-12-25 17:37:54 +0200

    filters out only normal naming format images

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use regex::Regex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rgx = Regex::new(r#"(_[0-1][0-9][0-3][0-9][0-9]{4}_).+(\.png|jpg)"#)
+    let rgx = Regex::new(r#"[0-9]+_([0-1][0-9][0-3][0-9][0-9]{4})_.+(\.png|jpg)"#)
         .expect("Error constructing regex");
 
     let files = std::fs::read_dir("screenshots")
         .expect("Error opening dir")
         .filter(|f| {
             match f {
-                Ok(file) => rgx.is_match(file.file_name())
+                Ok(file) => {
+                    let name = file.file_name();
+                    rgx.is_match(format!("{:?}", name).as_str())
+                },
+                Err(e) => {
+                    println!("{:?}", e);
+                    false
+                },
             }
         });
     
